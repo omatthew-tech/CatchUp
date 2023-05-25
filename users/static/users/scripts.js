6606e611e9d4fa0da88416154d4f6b1e1853a6d7
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2023-05-24 21:20:04 -0500

    added dropdown menu

--- users/static/users/scripts.js
+++ users/static/users/scripts.js
@@ -23,6 +23,26 @@ function changeName() {
 
 setInterval(changeName, 5000); // Change the name every 5 seconds
 
+// Dropdown Menu
+function toggleDropdown() {
+    document.getElementById("myDropdown").querySelector(".user-dropdown-content").classList.toggle("show");
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("user-dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 
 
 
